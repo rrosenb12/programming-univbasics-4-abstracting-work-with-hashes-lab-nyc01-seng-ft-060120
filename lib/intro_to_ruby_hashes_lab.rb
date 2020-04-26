2826930a7fe5a01869ade4d084866e5639ec2b9f
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-04-26 19:18:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,7 @@
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
   my_hash_creator_hash = {:id => value}
+  return my_hash_creator_hash
 end
 
 def read_from_hash(hash, key)
